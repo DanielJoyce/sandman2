acbc27567f9b32793958bc650523ab01025032c7
daniel <daniel@vizualiiz.com>
2015-09-11 14:25:59 -0700

    Added test for per_page support

--- tests/test_extended_functionality.py
+++ tests/test_extended_functionality.py
@@ -7,3 +7,10 @@ def test_pagination(client):
     assert res.status_code == 200
     assert len(res.json['resources']) == 20
     assert res.json['resources'][0]['ArtistId'] == 21
+
+def test_pagination_per_page(client):
+    """If we request paginated results, is per_page respected?"""
+    res = client.get('/artist?page=2&per_page=4')
+    assert res.status_code == 200
+    assert len(res.json['resources']) == 4
+    assert res.json['resources'][0]['ArtistId'] == 5
